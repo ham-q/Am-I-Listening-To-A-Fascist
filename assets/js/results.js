1173bc63b393a09a328f4e0d502ae8e8dc5747c4
Hamzah Qureshi <hamzahqureshi25@gmail.com>
2020-10-15 22:56:20 +0100

    fixed typo

--- assets/js/results.js
+++ assets/js/results.js
@@ -6,7 +6,7 @@ var net = localStorage.getItem("neutralScore").split(",");
 var antifa = localStorage.getItem("antifascistScore").split(",");
 
 const results = [
-    "The person in question, while perhaps not fully a fascist, definitely seems to lean heavily on their side. One need not hit all the points to be one after, as fascism is quite a varied set of beliefs. I'd suggest you be wary of what they have to say and, if possible be critical of any points relating to the points presented in the quiz.",
+    "The person in question, while perhaps not fully a fascist, definitely seems to lean heavily on their side. One need not hit all the points to be one after all, as fascism is quite a varied set of beliefs. I'd suggest you be wary of what they have to say and, if possible be critical of any points relating to the points presented in the quiz.",
     "It seems they are in the middle, neither fascist nor antifascist (or perhaps holds a mix of the two). While not fascists directly, those who straddle the fence may easily fall into or copy fascist talking points, given their lack of rejection. Be critical of both them and all content creator you consume and what they're putting out there. If it's anything like the 14 points mentioned in the quiz, you'll know what you're dealing with.",
     "Congrats, it seems the person in question is very much to opposite of a fascist. You needn't worry about their takes bringing you further to the ideology, though I'd still suggest you be critical of both them and all content creators you consume to understand what they're feeding you, especially if it's anything close to the 14 points shown in the quiz."
 ]
